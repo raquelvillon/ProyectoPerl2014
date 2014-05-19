28504f0354d3848c197bfd36bfef0b3b7b607f80
Raquel Villon <raqevill@espol.edu.ec>
2014-05-18 23:10:45 -0500

    Reescritura del pl. encuentra información básica
    reescribí el código para que lea directamente el archivo. primero lee
    lineas, luego separa las oraciones y en cada oración busca lo que
    necesita.

--- prueba.pl
+++ prueba.pl
@@ -1,13 +1,27 @@
-  $archivo=<>; #abre un txt que se especifica cuando se llama al .perl
+#abre un txt y guarda las lineas en el array informacion
+open (datos,"informacion.txt");
+@informacion=<datos>;
+close (datos);
+#de cada linea en @informacion separa las cadenas separadas por una coma
+foreach (@informacion){
+	@oraciones = split(/\,+/, $informacion[0]);
+}
+#hash que maneja la información del estudiante
+%estudiante=(nombre,"",estudio,"",direccion,"inicia");
 
-  @oraciones;
-#Guarda una oración separada por una coma
+foreach $oracion (@oraciones){
+	#print $oracion;
+	####### -----PRUEBAS---######
+	#busqueda del nombre
+	if ($oracion =~ /nombre es ([0-9a-zA-Z\s]+)*/) {
+		$estudiante{nombre}=$1;
+	}
+	if ($oracion =~ /estudio en la ([0-9a-zA-Z]+)/) {
+		$estudiante{estudio}=$1;
+	}
+	if ($oracion =~ /vivo en ([0-9a-zA-Z]+)/) {
+		$estudiante{direccion}=$1;
+	}
+}
+print "Nombre: $estudiante{nombre} \nEstudios: $estudiante{estudio} \nDireccion: $estudiante{direccion} ";
 
-  if ($archivo =~ /([0-9a-zA-Z\s]+)*\,/) 
-  { 
- 	push(@oraciones,$1);
-  	print @oraciones
-  }else{
-  	print "no lo contiene";
-  	
-  }
