e004298bb6accefd7ec5be5724117cb8f8debac1
madasanc <marcelo_sanchez_1991@hotmail.com>
2014-06-03 10:02:31 -0500

    Búsqueda de Estaturas en el dataset
    Test para encontrar las estaturas dentro del dataset, conversión de las
    estaturas encontradas de centímetros a metros.

---
+++ tests/test_estatura.pl
@@ -0,0 +1,32 @@
+use utf8; #permite que se compilen caracteres especiales como las vocales tildadas y las eñes
+open (datos,"<:encoding(UTF-8)","dataset.txt"); #hace que lea el txt tal y como esta escrito, incluidas los caracteres esperciales
+
+@informacion=<datos>;
+close (datos);
+open (sal,">salidaEstatura.txt");
+foreach $inf(@informacion){
+	
+	if (length($inf)>20) {
+		push(@parrafos,$inf);
+	}
+}
+$numParrafos=length(@parrafos);
+
+foreach $info(@parrafos){
+	push(@oraciones,split(/\,+/, $info));
+}
+ 
+foreach $estatura (@oraciones){
+	#Encuentra las estaturas en metros
+	if ($estatura =~ /([mM]ido |[mM]i estatura es de |[mM]i estatura es |[mM]ido aproximadamente )([1]+\.)+([0-9]{2}|[0-9])+(m| m|metros| metros)*/){
+		print sal "Estatura: ".$2.$3." metros\n";
+	}
+	#Encuentra las estaturas en centimetros
+	if ($estatura =~ /([mM]ido |[mM]i estatura es de |[mM]i estatura es |[mM]ido aproximadamente )([0-9]{3})+(cm| cm|centimetros| centimetros)*/){
+		$datoestatura = $2/100;
+		print sal "Estatura: ".$datoestatura." metros\n";
+	}
+	#print "\n";
+}     
+
+close (sal);
