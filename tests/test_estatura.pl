96f3cfd64529dc18b0484140cf2b923a3349ffdf
madasanc <marcelo_sanchez_1991@hotmail.com>
2014-06-03 15:28:45 -0500

    Cambio de las estaturas, a dos decimas
    Corrección, todas las estaturas encontradas ahora salen con dos cifras
    decimales.

--- tests/test_estatura.pl
+++ tests/test_estatura.pl
@@ -19,7 +19,12 @@ foreach $info(@parrafos){
 foreach $estatura (@oraciones){
 	#Encuentra las estaturas en metros
 	if ($estatura =~ /([mM]ido |[mM]i estatura es de |[mM]i estatura es |[mM]ido aproximadamente )([1]+\.)+([0-9]{2}|[0-9])+(m| m|metros| metros)*/){
-		print sal "Estatura: ".$2.$3." metros\n";
+		$datoestatura = $2.$3;
+		if (length($datoestatura)<4){
+			$datoestatura = $datoestatura."0";
+		}
+		print sal "Estatura: ".$datoestatura." metros\n";
+		#print sal "Estatura: ".$2.$3." metros\n";
 	}
 	#Encuentra las estaturas en centimetros
 	if ($estatura =~ /([mM]ido |[mM]i estatura es de |[mM]i estatura es |[mM]ido aproximadamente )([0-9]{3})+(cm| cm|centimetros| centimetros)*/){
